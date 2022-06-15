5e6ba07be65c4227128eb7ea983759aa64929a04
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-06-15 20:04:45 +0530

    table setup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import React from "react";
-import { useCSVReader } from "react-papaparse";
+
 const App = () => {
-  const CSVReader = useCSVReader();
   return (
     <>
       <div>App</div>
-      <CSVReader
-        onUploadAccepted={(results) => {
-          console.log("---------------------------");
-          console.log(results);
-          console.log("---------------------------");
-        }}
-        onError={(error) => console.log(error)}
-      ></CSVReader>
     </>
   );
 };
