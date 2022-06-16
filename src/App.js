95a9e7f572e8f585773ce6eea8cd75325ed6f611
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-06-16 09:46:01 +0530

    form added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import cities from "./citydetails";
+import Form from "./components/Form/Form1";
 import BasicTable from "./Table";
 const App = () => {
   return (
     <>
       <div>App</div>
-
+      <Form />
       <BasicTable data1={cities} />
     </>
   );
---
+++ src/components/Form/Form1.js
@@ -0,0 +1,94 @@
+import React, { useState, useEffect } from "react";
+import { TextField, Button, Typography, Paper } from "@material-ui/core";
+
+import useStyles from "./styles";
+
+const Form = ({ currentId, setCurrentId }) => {
+  const [postData, setPostData] = useState({
+    title: "",
+    message: "",
+    tags: "",
+    selectedFile: "",
+  });
+
+  const classes = useStyles();
+
+  useEffect(() => {});
+
+  const clear = () => {
+    setCurrentId(0);
+    setPostData({ title: "", message: "", tags: "", selectedFile: "" });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    console.log(e);
+  };
+
+  return (
+    <Paper className={classes.paper} elevation={6}>
+      <form
+        autoComplete="off"
+        noValidate
+        className={`${classes.root} ${classes.form}`}
+        onSubmit={handleSubmit}
+      >
+        <Typography variant="h6">
+          {currentId ? "Editing" : "Creating"} a Memory{" "}
+        </Typography>
+
+        <TextField
+          name="title"
+          variant="outlined"
+          label="Title"
+          fullWidth
+          value={postData.title}
+          onChange={(e) => setPostData({ ...postData, title: e.target.value })}
+        />
+        <TextField
+          name="message"
+          variant="outlined"
+          label="Message"
+          fullWidth
+          value={postData.message}
+          onChange={(e) =>
+            setPostData({ ...postData, message: e.target.value })
+          }
+        />
+        <TextField
+          name="tags"
+          variant="outlined"
+          label="Tags (coma seperated)"
+          fullWidth
+          value={postData.tags}
+          onChange={(e) =>
+            setPostData({ ...postData, tags: e.target.value.split(",") })
+          }
+        />
+
+        <Button
+          className={classes.buttonSubmit}
+          variant="contained"
+          color="primary"
+          size="large"
+          type="submit"
+          fullWidth
+        >
+          Submit
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          size="small"
+          onClick={clear}
+          fullWidth
+        >
+          Clear
+        </Button>
+      </form>
+    </Paper>
+  );
+};
+
+export default Form;
